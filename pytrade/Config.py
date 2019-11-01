03babe3823895e20374ae853566c0a7568274e93
DmitryPukhov <dmitry.pukhov@gmail.com>
2019-11-01 10:35:01 +0300

    Config changed

--- pytrade/Config.py
+++ pytrade/Config.py
@@ -7,7 +7,7 @@ class Config:
     server = 'junior.webquik.ru'
     conn = 'wss://' + server + ':443/quik'
     account = 'U0149094'
-    passwd = '02008'
+    passwd = '12008'
 
     # Main asset
     sec_class = 'QJSIM'
