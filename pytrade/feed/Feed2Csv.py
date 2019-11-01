ef29a5b9a04f1cdafaf8c364036c3bf62e4adf7d
DmitryPukhov <dmitry.pukhov@gmail.com>
2019-11-01 11:23:25 +0300

    Csv writing bugs fixed

--- pytrade/feed/Feed2Csv.py
+++ pytrade/feed/Feed2Csv.py
@@ -9,7 +9,7 @@ class Feed2Csv(BaseFeed):
     Receive ticks and level 2 and persist to csv
     """
     _logger = logging.getLogger(__name__)
-    _logger.setLevel(logging.INFO)
+    _logger.setLevel(logging.DEBUG)
 
     def __init__(self, feed, sec_class, sec_code, data_dir='./data'):
         super().__init__(feed, sec_class, sec_code)
@@ -17,35 +17,40 @@ class Feed2Csv(BaseFeed):
         self._write_interval = dt.timedelta(minutes=1)
         self._data_dir = data_dir
         self._logger.info("Candles and level2 will be persisted each %s to %s", self._write_interval, self._data_dir)
+        self._last_write_time = dt.datetime.min
 
     def write(self):
         """
         Append collected data to csv. File name contains asset and day. Each day in each file.
         """
         # Get time from first element. Index: (asset, datetime)
-        start_time = self.candles.index[0][0]
-        file_name = '%s_%s_%s.csv' % (self.sec_class, self.sec_code, start_time.strftime('%Y-%m-%d'))
 
         # Write then clear candles
-        file_path = os.path.join(self._data_dir, file_name)
-        self._logger.debug("Writing candles to %s", file_path)
-        self.candles.to_csv(file_path, mode='a', header=False)
-        self.candles = self.candles.iloc[0:0]
+        if not self.candles.empty:
+            start_time = self.candles.index[0][0]
+            file_name = '%s_%s_%s.csv' % (self.sec_class, self.sec_code, start_time.strftime('%Y-%m-%d'))
+            file_path = os.path.join(self._data_dir, file_name)
+            self._logger.debug("Writing candles to %s", file_path)
+            self.candles.to_csv(file_path, mode='a', header=False)
+            self.candles = self.candles.iloc[0:0]
+            self._last_write_time = dt.datetime.now()
 
         # Write then clear level2
-        start_time = self.level2.index[0][0]
-        file_name = '%s_%s_level2_%s.csv' % (self.sec_class, self.sec_code, start_time.strftime('%Y-%m-%d'))
-        file_path = os.path.join(self._data_dir, file_name)
-        self._logger.debug("Writing level2 to %s", file_path)
-        self.level2.to_csv(file_path, mode='a', header=False)
-        self.level2 = self.level2.iloc[0:0]
+        if not self.level2.empty:
+            start_time = self.level2.index[0][0]
+            file_name = '%s_%s_level2_%s.csv' % (self.sec_class, self.sec_code, start_time.strftime('%Y-%m-%d'))
+            file_path = os.path.join(self._data_dir, file_name)
+            self._logger.debug("Writing level2 to %s", file_path)
+            self.level2.to_csv(file_path, mode='a', header=False)
+            self.level2 = self.level2.iloc[0:0]
+            self._last_write_time = dt.datetime.now()
 
     def on_heartbeat(self):
         """
         Heartbeat received
         """
         # If time interval elapsed, write dataframe to csv
-        if (dt.datetime.now() - self._last_heartbeat) > self._write_interval:
+        if (dt.datetime.now() - self._last_write_time) > self._write_interval:
             self.write()
 
         # Store current time as last heart beat
